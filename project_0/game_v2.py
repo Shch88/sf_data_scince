01d9757a21b1e63a7b8f5222237c08a482cf8f69
Shch <alexshelkunov@yandex.ru>
2024-10-05 15:08:00 +0300

    new game_v2.py

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -4,7 +4,7 @@
 import numpy as np
 
 def random_predict(number:int=1) -> int:
-    """Угадываем число при помощи встроенного рандома библиотеки numpy
+    """Угадываем число халвингом списка
 
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
@@ -14,14 +14,30 @@ def random_predict(number:int=1) -> int:
     """
     
     count = 0
+    numbers_list = list(range(1,101)) # Создадим список возможных чисел
+    count_list = [] # Для "отлова" верного результата реверсивной ф-ции     
     
-    while True:
-        count += 1
-        predict_number = np.random.randint(1, 101)
-        if number == predict_number:
-            break
-    return count
-
+    def halved_predict_func(numbers_list, count): # Функция поиска числа халвингом
+        count +=1
+        if number != numbers_list[0] and number != numbers_list[-1]: # Исключаем первое и последнее числа
+            ind = len(numbers_list)//2 #  Индекс списка (середина)
+            predict_number = numbers_list[ind]  # Определяем точку халвинга (угадываем число)
+            if number == predict_number:
+                count_list.append(count)
+            elif number < predict_number:
+                numbers_list = numbers_list[1:ind] # Халвим список
+                halved_predict_func(numbers_list, count) # Продолжаем поиск с новым списком
+            else:
+                ind += 1
+                numbers_list = numbers_list[ind:-1] # Халвим список
+                halved_predict_func(numbers_list, count) # Продолжаем поиск с новым списком
+        elif number == numbers_list[0]: # Подразумеваем, что отгадали )
+            count_list.append(count)
+        else: # Подразумеваем, что отгадали )
+            count_list.append(count) 
+        return count_list[0]
+    result = halved_predict_func(numbers_list, count)
+    return result
 #print(f'Количество попыток: {random_predict()}')
 
 def score_game(random_predict) -> int:
